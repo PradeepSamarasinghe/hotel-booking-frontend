745e193553f36b176b236e137bb34d09470ff5b1
PradeepSamarasinghe <it22608086@my.sliit.lk>
2025-12-04 11:58:18 +0530

    update user data
    updated user data adding useState and useEffect

--- src/components/userData/userdata.jsx
+++ src/components/userData/userdata.jsx
@@ -1,11 +1,16 @@
 import axios from "axios";
+import { useState , useEffect} from "react";
 function UserTag(props){
     // console.log(props);
     // const token = localStorage.getItem("token");
-
-    const token = localStorage.getItem("token");
+    const [name, setName] = useState("");
+    const [userFound, setUserFound] = useState(false);
  
-    if(token != null){
+    useEffect(
+        () => {
+
+        const token = localStorage.getItem("token");
+        if(token != null){
         // console.log("UserTag Token:", token);
         axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/users/`, {
             headers: {
@@ -14,10 +19,17 @@ function UserTag(props){
             }
         }).then((res) => {
             console.log("UserTag User Data:", res.data);
+            setName(res.data.user.firstname + " " + res.data.user.lastName);
+            setUserFound(true);
         }).catch((err) => {
             console.log(err);
-        });
-    }
+        });  
+        }else{
+            setName("");
+            setUserFound(false);
+        }
+        },[userFound]
+    );
 
     return(
         <div className="bg-amber-200 absolute right-10 flex gap-2 items-center cursor-pointer">
@@ -25,7 +37,15 @@ function UserTag(props){
                 className ="w-10 h-10 rounded-full "
                 src={props.imageLink}
             />
-            <span className="text-white mr-2 ">{props.name}</span>
+            <span className="text-white mr-2 ">{name}</span>
+
+            <button onClick={() => {
+                localStorage.removeItem("token")
+                setName("");
+                setUserFound(false);
+                // const token = localStorage.getItem("token")
+                // console.log(token)
+            }}>Logout</button>
         </div>
     )
 }
