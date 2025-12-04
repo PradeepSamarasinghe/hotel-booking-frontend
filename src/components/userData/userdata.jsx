269c4408b07d55e3ed4ab709a9499a3b9f6649c0
PradeepSamarasinghe <it22608086@my.sliit.lk>
2025-12-04 09:23:43 +0530

    update userdata

--- src/components/userData/userdata.jsx
+++ src/components/userData/userdata.jsx
@@ -1,6 +1,23 @@
-
+import axios from "axios";
 function UserTag(props){
     // console.log(props);
+    // const token = localStorage.getItem("token");
+
+    const token = localStorage.getItem("token");
+ 
+    if(token != null){
+        // console.log("UserTag Token:", token);
+        axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/users/`, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            }
+        }).then((res) => {
+            console.log("UserTag User Data:", res.data);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
 
     return(
         <div className="bg-amber-200 absolute right-10 flex gap-2 items-center cursor-pointer">
